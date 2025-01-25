8cb9d0a1cd301ed27df565745ce20b8147bcdf19
spidgrou <maxt@maxt.it>
2025-01-25 16:13:02 +1200

    modified:   start.sh

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
 echo "Inizio Ricerca Seriale Venus"
-python3 /data/MQTTVenus-ID/auto_vrm_id.py
+python3 auto_vrm_id.py
 echo "auto_vrm_id.py completato"
 
 echo "Inizio Ricerca Solar IDs"
-python3 /data/MQTTVenus-ID/find_ids.py
+python3 find_ids.py
 echo "find_ids.py completato"
 
 echo "Esecuzione completata!"
