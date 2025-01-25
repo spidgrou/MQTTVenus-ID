f721879d183eb8911c4864044da585a2f1c1cdae
spidgrou <maxt@maxt.it>
2025-01-25 16:06:42 +1200

    Changes to be committed: 	modified:   start.sh     Aggiunto percorso /data/MQTTVenus-ID

--- start.sh
+++ start.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 
-echo "Inizio esecuzione di auto_vrm_id.py"
-python3 auto_vrm_id.py
+echo "Inizio Ricerca Seriale Venus"
+python3 /data/MQTTVenus-ID/auto_vrm_id.py
 echo "auto_vrm_id.py completato"
 
-echo "Inizio esecuzione di find_ids.py"
-python3 find_ids.py
+echo "Inizio Ricerca Solar IDs"
+python3 /data/MQTTVenus-ID/find_ids.py
 echo "find_ids.py completato"
 
 echo "Esecuzione completata!"
